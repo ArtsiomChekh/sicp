abd4fbebf6bdd933f57585586fd5e263153481ca
artsiomchekh <qbartem@gmail.com>
2023-08-30 11:56:16 +0300

    add ex. 1.6

---
+++ main/ex_1.6.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(define (improve guess x)
+        (average guess (/ x guess)))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+(define (square x)
+  (* x x))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+; New version of 'if'
+;(define (new-if predicate then-clause else-clause)
+;  (cond (predicate then-clause)
+;        (else else-clause)))
+;(define (sqrt-iter guess x)
+;  (new-if (good-enough? guess x)
+;          guess
+;          (sqrt-iter (improve guess x) x)))
+
+
+; Solution: If we rewrite the square root with new-if, we will get an out-of-memory error
+; because new-if is implemented as applicative order evaluation. Thus, a function's arguments
+; are evaluated before the function is applied. 
