03c447e33db49b5e587673bfb1866c1d05e7ba19
artsiomchekh <qbartem@gmail.com>
2023-09-06 10:05:35 +0300

    Added tests for ex_1.3

--- src/main/ex_1.3.rkt
+++ src/main/ex_1.3.rkt
@@ -1,7 +1,12 @@
 #lang racket
+
+(provide square sum-of-squares sum-of-squares-of-largest)
+
 (define (square x) (* x x))
+
 (define (sum-of-squares a b) (+ (square a) (square b)))
+
 (define (sum-of-squares-of-largest a b c)
   (cond ((and (>= b a) (>= c a)) (sum-of-squares b c))
-           ((and (>= a b) (>= c b)) (sum-of-squares a c))
-           (else (sum-of-squares a b))))
+        ((and (>= a b) (>= c b)) (sum-of-squares a c))
+        ((and (>= a c) (>= b c)) (sum-of-squares a b))))
---
+++ src/test/ex_1.3_test.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(require rackunit
+         "../main/ex_1.3.rkt")
+
+(define counter 0)
+
+(define (report-success description)
+  (set! counter (+ counter 1))
+  (printf "Test ~a passed: ~a\n" counter description))
+
+(check-equal? (square 2) 4 "Square")
+(report-success "Square of 2 is 4")
+
+(check-equal? (square 4) 16 "Square")
+(report-success "Square of 4 is 16")
+
+(check-not-equal? (square 4) 15 "Square is not equal to 15")
+(report-success "Square of 4 is not 15")
+
+(check-equal? (sum-of-squares 2 4) 20 "Sum-of-squares")
+(report-success "Sum-of-squares of 2 and 4 is 20")
+
+(check-equal? (sum-of-squares 5 5) 50 "Sum-of-squares")
+(report-success "Sum-of-squares of 5 and 5 is 50")
+
+(check-equal? (sum-of-squares-of-largest 2 4 5) 41 "Sum-of-squares-of-largest")
+(report-success "Sum-of-squares-of-largest of 2 4 5 is is 41")
+
+(check-equal? (sum-of-squares-of-largest 6 1 8) 100 "Sum-of-squares-of-largest")
+(report-success "Sum-of-squares-of-largest of 6 1 8 is is 100")
