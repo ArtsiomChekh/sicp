8986ba8a0d742e7d3f905d2fe4f551406d066ca5
artsiomchekh <qbartem@gmail.com>
2023-09-06 10:51:49 +0300

    Update 1.7

--- src/main/ex_1.7.rkt
+++ src/main/ex_1.7.rkt
@@ -1,37 +1,26 @@
 #lang racket
+
 (define (square x)
   (* x x))
 
-(define (sqrt x)
-  (sqrt-iter 1.0 x))
-
 (define (improve guess x)
-        (average guess (/ x guess)))
+  (average guess (/ x guess)))
 
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+(define (new-sqrt-iter prev-guess guess x)
+  (if (new-good-enough? prev-guess guess)
       guess
-      (sqrt-iter (improve guess x) x)))
+      (new-sqrt-iter guess (improve guess x) x)))
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+(define (new-sqrt x)
+  (new-sqrt-iter 0 1.0 x))
+
+(define (new-good-enough? prev-guess guess)
+  (< (abs (- guess prev-guess))
+     0.00001))
 
 ; Solution:
-; ***
-; Computing small numbers < 0.001 (sqrt 0.0000000001) gives us result 0.03125000106562499.
-; After this we compute (square 0.03125000106562499) and we will get the not precise enough
-; result 0.000976562566601563 < 0.001
-; Computing very large numbers (sqrt 11111111111111111111111111111111111111) gives us infinite evaluating.
-; ***
-; An altetnative stratege for implementing good-enough:
-; ----------------------------------------------------------
-; (define (good-enough? guess x)
-;  (< (abs (- guess (improve guess x))) (/ guess 1000000)))
-; ----------------------------------------------------------
-; Computing small numbers < 0.001 (sqrt 0.0000000001) gives us result 1.0000000015603234e-5
-; Computing very large numbers (sqrt 11111111111111111111111111111111111111) gives us result 3.33333333495636e+18
-; This new implementing works better with very large number. But evantually floating point numbers have limited precision,
-; which means that numbers can result in a loss of precision.
+; A new strategy for implementing "good enough"? allows us to evaluate small
+; and a cumbersome number with no infinite exceptions.
